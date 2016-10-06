20f86782d72e3c7accd4de07f61ab1a1e74dca72
team224 <phsrobotics224@gmail.com>
2016-10-06 19:39:12 -0400

    Update init.js

--- js/init.js
+++ js/init.js
@@ -9,5 +9,5 @@ $('.modal-trigger').leanModal();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-var audio = new Audio('audio_file.mp3');
+var audio = new Audio('z.mp3');
 audio.play();
